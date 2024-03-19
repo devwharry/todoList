7db23026ebcc0765279b4e2d6a55de62f2b47ba2
wharry <devwharry@gmail.com>
2024-03-18 23:04:45 -0300

    criado a função de ordem alfabética do app

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,4 @@
-const Filter = ({filter, setFilter}) => {
+const Filter = ({ filter, setFilter, setSort }) => {
   return (
     <div className="filter">
         <h2>Filter:</h2>
@@ -14,8 +14,8 @@ const Filter = ({filter, setFilter}) => {
 
             <div>
                 <p>Ordem alfabética:</p>
-                <button>Asc</button>
-                <button>Desc</button>
+                <button onClick={() => {setSort("Asc")}}>Asc</button>
+                <button onClick={() => {setSort("Desc")}}>Desc</button>
             </div>
         </div>
     </div>
