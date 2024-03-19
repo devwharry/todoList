fe94cc44c8ea5cb8a211381b8f1d4ce7658eab31
wharry <devwharry@gmail.com>
2024-03-18 22:56:08 -0300

    criado a função filter

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,9 @@ function App() {
 
   const [search, setSearch] = useState("");
 
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
   const addTodo = (text, category) => {
     const newTodos = [...todos, {
       id: Math.floor(Math.random() * 10000),
@@ -62,9 +65,16 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} />
       <div className="todo-list">
         {todos
+        .filter((todo) => 
+          filter === "All"
+          ? true
+          : filter === "Completed"
+          ? todo.isCompleted
+          : !todo.isCompleted
+        )
         .filter((todo) =>
           todo.text.toLowerCase().includes(search.toLowerCase())
         )
