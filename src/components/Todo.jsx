a1b9582e6387e94ff6251713d788eefeef1fd02f
wharry <devwharry@gmail.com>
2024-03-18 22:11:18 -0300

    criada a função completeTodo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div 
+        className="todo" 
+        style={{textDecoration: todo.isCompleted ? "line-through" : ""}}
+    >
         <div className="content">
             <p>{todo.text}</p>
             <p className="category">
@@ -10,8 +13,10 @@ const Todo = ({ todo, removeTodo }) => {
             </p>
         </div>
         <div>
-            <button className='complete'>Completar</button>
-            <button className='remove' onClick={()=>removeTodo(todo.id)}>
+            <button className='complete' onClick={() => completeTodo(todo.id)}>
+                Completar
+            </button>
+            <button className='remove' onClick={() => removeTodo(todo.id)}>
                 x
             </button>
         </div>
